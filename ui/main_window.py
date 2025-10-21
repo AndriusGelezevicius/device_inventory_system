75721899f75569965c8699c068132aa9fb1a96c9
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-10-21 19:21:21 +0300

    created basic table

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,15 +1,24 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QPushButton, QLineEdit, QDateEdit, \
-    QSpinBox, QTableWidgetItem, QLabel
+    QSpinBox, QTableWidgetItem, QLabel, QHeaderView
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Device Inventory System")
-        self.setGeometry(200, 100, 1300, 800)
+        self.setFixedSize(1300, 800)
         self.setup_ui()
 
     def setup_ui(self):
         self.label_plan = QLabel("Plan", self)
-        # self.label_plan.setGeometry(50,30,300,50)
+        self.label_plan.setGeometry(500,30,300,50)
         self.label_plan.setObjectName("label_plan")
+
+        self.table = QTableWidget(10,3, self)
+        self.table.setHorizontalHeaderLabels(["Name", "Date", "Bank"])
+        self.table.setGeometry(50,80,1000,650)
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.table.setObjectName("table")
+
+
