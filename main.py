cad8e9e5945a3f948b5b8c241333d94f6e142f28
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-10-21 19:04:36 +0300

    created style.qss

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
-from PyQt6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
 
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    with open("ui/style.qss", "r") as f:
+        style = f.read()
+        app.setStyleSheet(style)
+
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
--- ui/main_window.py
+++ ui/main_window.py
@@ -1,5 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QPushButton, QLineEdit, QDateEdit, \
-    QSpinBox, QTableWidgetItem
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QPushButton, QLineEdit, QDateEdit, \
+    QSpinBox, QTableWidgetItem, QLabel
 
 
 class MainWindow(QWidget):
@@ -7,7 +7,9 @@ class MainWindow(QWidget):
         super().__init__()
         self.setWindowTitle("Device Inventory System")
         self.setGeometry(200, 100, 1300, 800)
-       # self.setup_ui(self)
+        self.setup_ui()
 
-   # def setup_ui(self):
-
+    def setup_ui(self):
+        self.label_plan = QLabel("Plan", self)
+        # self.label_plan.setGeometry(50,30,300,50)
+        self.label_plan.setObjectName("label_plan")
