fb9a336a944729a8c8a15b0d887ce124f4895e6e
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-10-26 13:14:53 +0200

    add combobox

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QPushButton, QLineEdit, QDateEdit, \
-    QSpinBox, QTableWidgetItem, QLabel, QHeaderView
+    QSpinBox, QTableWidgetItem, QLabel, QHeaderView, QComboBox
 
 
 class MainWindow(QWidget):
@@ -11,12 +11,12 @@ class MainWindow(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-
+        # --- Label ---
         self.label_plan = QLabel("Plan")
         self.label_plan.setObjectName("label_plan")
         self.label_plan.setAlignment(Qt.AlignCenter)
 
-
+        # --- Table ---
         self.table = QTableWidget(10, 3)
         self.table.setObjectName("table")
         self.table.setHorizontalHeaderLabels(["Name", "Date", "Bank"])
@@ -27,14 +27,21 @@ class MainWindow(QWidget):
         left_layout.addWidget(self.label_plan)
         left_layout.addWidget(self.table)
 
+        # --- Right column: button + dropdown ---
         self.button_add = QPushButton("Add Record")
-       # self.button_add.setObjectName("btn_add")
 
+        self.dropdown = QComboBox()
+        self.dropdown.setObjectName("dropdown_devices")
+        self.dropdown.addItems(["Router", "Switch", "Server", "Laptop"])
 
         right_layout = QVBoxLayout()
-        right_layout.addWidget(self.button_add,alignment=Qt.AlignTop)
+        right_layout.setAlignment(Qt.AlignTop)
+        right_layout.setSpacing(10)
+        right_layout.setContentsMargins(10, 40, 10, 10)
+        right_layout.addWidget(self.button_add)
+        right_layout.addWidget(self.dropdown)
 
-
+        # --- Main layout ---
         main_layout = QHBoxLayout()
         main_layout.addLayout(left_layout, stretch=3)
         main_layout.addLayout(right_layout, stretch=1)
