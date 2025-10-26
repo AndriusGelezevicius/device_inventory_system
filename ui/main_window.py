e874854b1b613e20996b48dafd205551fd0e3d19
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2025-10-26 13:02:49 +0200

    add btn_add

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QPushButton, QLineEdit, QDateEdit, \
     QSpinBox, QTableWidgetItem, QLabel, QHeaderView
 
@@ -10,15 +11,35 @@ class MainWindow(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        self.label_plan = QLabel("Plan", self)
-        self.label_plan.setGeometry(500,30,300,50)
-        self.label_plan.setObjectName("label_plan")
 
-        self.table = QTableWidget(10,3, self)
+        self.label_plan = QLabel("Plan")
+        self.label_plan.setObjectName("label_plan")
+        self.label_plan.setAlignment(Qt.AlignCenter)
+
+
+        self.table = QTableWidget(10, 3)
+        self.table.setObjectName("table")
         self.table.setHorizontalHeaderLabels(["Name", "Date", "Bank"])
-        self.table.setGeometry(50,80,1000,650)
         header = self.table.horizontalHeader()
         header.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
-        self.table.setObjectName("table")
+
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.label_plan)
+        left_layout.addWidget(self.table)
+
+        self.button_add = QPushButton("Add Record")
+       # self.button_add.setObjectName("btn_add")
+
+
+        right_layout = QVBoxLayout()
+        right_layout.addWidget(self.button_add,alignment=Qt.AlignTop)
+
+
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(left_layout, stretch=3)
+        main_layout.addLayout(right_layout, stretch=1)
+
+
+        self.setLayout(main_layout)
 
 
